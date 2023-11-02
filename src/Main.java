35a4a170fec9634df1cce7fbbc64e908fb42b189
abdullah <apoo0809@gnail.com>
2023-11-02 16:53:29 +0300

    ilk commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 22;
+        if (sayi < 20) {
+            System.out.println("Sayı 20 den küçüktür");
+        }else if (sayi==20) {
+            System.out.println("Sayı 20 ye eşittir");
+        }else{
+            System.out.println("Sayı 20 den büyüktür  ");
+            }}
+        }
